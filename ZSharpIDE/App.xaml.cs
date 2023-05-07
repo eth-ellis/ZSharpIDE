39dbd27d9ad14c84f7f499e1ee77543e78180b6f
Ethan <ethanellis@adam.com.au>
2023-05-07 21:21:17 +0930

    Add project files.

---
+++ ZSharpIDE/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace ZSharpIDE
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        private Window mainWindow;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            this.mainWindow = new MainWindow();
+            this.mainWindow.Activate();
+        }
+    }
+}
